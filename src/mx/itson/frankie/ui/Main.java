046921b402b76c5c3d13258fd576d1a227dbf0e8
Angel <Angel@10.232.9.23>
2022-10-04 11:22:00 -0700

    Se implemento una nueva clase

--- src/mx/itson/frankie/ui/Main.java
+++ src/mx/itson/frankie/ui/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
 import mx.itson.frankie.entidades.Ingrediente;
 import mx.itson.frankie.entidades.Paso;
 import mx.itson.frankie.entidades.Receta;
+import mx.itson.frankie.entidades.Usuario;
 import mx.itson.frankie.enumerador.Dificultad;
 
 /**
@@ -25,7 +26,7 @@ public class Main {
         
         Receta receta = new Receta();
         receta.setNombre("Frankie Brownie");
-        receta.setDescripcion("Hornea ina receta de FRANKIE BROWNIE con...");
+        receta.setDescripcion("Hornea una receta de FRANKIE BROWNIE con...");
         receta.setNumeroPorciones(6);
         receta.setDuracion(62);
         
@@ -42,14 +43,83 @@ public class Main {
         
         receta.setIngredientes(ingredientes);
         
-        Paso paso = new Paso();
-        paso.setOrden(6);
-        paso.setDescripcion("");
+        List<Paso> pasos = new ArrayList<>();
         
-        //pasos
+        Paso paso1 = new Paso();
+        paso1.setOrden(1);
+        paso1.setDescripcion("Horno precalentado a 180 °C.");
+        pasos.add(paso1);
+        
+        Paso paso2 = new Paso();
+        paso2.setOrden(2);
+        paso2.setDescripcion("Mezcla la harina para brownies chocolate Fudge de Betty Crocker® con el agua, "
+                + "el aceite y los huevos. Vierte la mezcla en un molde cuadrado de 30 x 20 cm previamente en harinado "
+                + "y hornea a 180 °C durante 25 minutos. Retira del horno y enfría.");
+        pasos.add(paso2);
+        
+        Paso paso3 = new Paso();
+        paso3.setOrden(3);
+        paso3.setDescripcion("Bate el queso crema con la Leche Condensada LA LECHERA®, "
+                + "la ralladura de limón, la mantequilla y un poco de colorante hasta integrar por completo y refrigera por 20 minutos.");
+        pasos.add(paso3);
+        
+        Paso paso4 = new Paso();
+        paso4.setOrden(4);
+        paso4.setDescripcion("Desmolda y corta en cuadros. Unta los brownies con un poco de betún; dibuja el cabello y la boca con el chocolate fundido, "
+                + "coloca los ojos de azúcar y la nariz con las lunetas. Ofrece.");
+        pasos.add(paso4);
+        
+        //pasos siguientes
+        
+        receta.setPasos(pasos);
         
         receta.setDificultad(Dificultad.LEGENDARIO);
-       
+        
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Jorge Espejel");
+        usuario.setEmail("eljorge@correo.com");
+        usuario.setPassword("rogramacion123");
+        
+        receta.setUsuario(usuario);
+        
+        
+        
+        System.out.println("***RECETA NESTLE***");   
+        System.out.println("Subida por:");
+        System.out.println(receta.getUsuario().getNombre() + " (" + receta.getUsuario().getEmail()+")");
+        
+        System.out.println("");
+        
+        System.out.println(receta.getNombre());
+        
+        System.out.println(receta.getDescripcion());
+        
+        System.out.println("");
+        
+        System.out.println("Porciones:");
+        System.out.println(receta.getNumeroPorciones());
+        
+        System.out.println("");
+        
+        System.out.println("Tiempo de preparacion:");
+        System.out.println(receta.getDuracion() + " minutos");
+        
+        System.out.println("");
+        
+        System.out.println("Ingredientes:");
+      
+        for(Ingrediente i : receta.getIngredientes()){
+            System.out.println(i.getNombre());
+        }
+        
+        System.out.println("");
+        
+        System.out.println("Pasos: ");
+        
+        for(Paso p : receta.getPasos()){
+            System.out.println(p.getOrden() + ".-" + p.getDescripcion());
+            
+        }
     }
     
 }
